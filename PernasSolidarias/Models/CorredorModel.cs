0c64fd81b958d1036a10ea6d74ad516b0d1f0254
maria.souza <maria.souza@fulltechpro.com.br>
2025-03-13 15:58:19 -0300

    #357 - adicionando Models

---
+++ PernasSolidarias/Models/CorredorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PernasSolidarias.Models
+{
+    public class CorredorModel
+    {
+        public string? Nome { get; set; }
+        public string? CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string? Email { get; set; }
+        public string? TamanhoBlusa { get; set; }
+        public string? Distancia { get; set; }
+        public string? UltimaCorrida { get; set; }
+    }
+}
